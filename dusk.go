c053bd7aa3af46edec9d0822c324b1404c3b76d4
Trevor Sawler <trevor.sawler@gmail.com>
2022-01-05 15:55:01 -0400

    cleanup

--- dusk.go
+++ dusk.go
@@ -14,7 +14,7 @@ func (c *Celeritas) ScreenShot(pageURL, testName string, w, h float64) {
 	page := rod.New().MustConnect().MustIgnoreCertErrors(true).MustPage(pageURL).MustWaitLoad()
 
 	img, _ := page.Screenshot(true, &proto.PageCaptureScreenshot{
-		Format: proto.PageCaptureScreenshotFormatJpeg,
+		Format: proto.PageCaptureScreenshotFormatPng,
 		Clip: &proto.PageViewport{
 			X:      0,
 			Y:      0,
